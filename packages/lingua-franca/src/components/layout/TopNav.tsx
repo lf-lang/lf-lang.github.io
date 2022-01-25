48af18248a6d5835fc120714382af70d0161f59e
Anirudh Rengarajan <44007330+arengarajan99@users.noreply.github.com>
2022-01-25 11:25:12 -0800

    Hotfix | 1/25/22

--- packages/lingua-franca/src/components/layout/TopNav.tsx
+++ packages/lingua-franca/src/components/layout/TopNav.tsx
@@ -17,59 +17,59 @@ import { OpenInMyLangQuickJump } from "./LanguageRecommendation";
 export const SiteNav = (props: Props) => {
   const i = createInternational<typeof navCopy>(useIntl())
   const IntlLink = createIntlLink(props.lang)
-  const loadDocSearch = () => {
-    const isDev = document.location.host.includes('localhost')
-    let customHandleSelected;
+  // const loadDocSearch = () => {
+  //   const isDev = document.location.host.includes('localhost')
+  //   let customHandleSelected;
 
-    if (isDev) {
-      customHandleSelected = (input, event, suggestion, datasetNumber, context) => {
-        const urlToOpen = suggestion.url.replace("www.lf-lang.github.io", "localhost:8000").replace("https", "http")
-        window.open(urlToOpen)
-      }
-    }
+  //   if (isDev) {
+  //     customHandleSelected = (input, event, suggestion, datasetNumber, context) => {
+  //       const urlToOpen = suggestion.url.replace("www.lf-lang.github.io", "localhost:8000").replace("https", "http")
+  //       window.open(urlToOpen)
+  //     }
+  //   }
 
 
-    // @ts-ignore - this comes from the script above
-    docsearch({
-      apiKey: '3c2db2aef0c7ff26e8911267474a9b2c',
-      indexName: 'typescriptlang',
-      inputSelector: '.search input',
-      handleSelected: customHandleSelected,
-    });
-  }
+  //   // @ts-ignore - this comes from the script above
+  //   // docsearch({
+  //   //   apiKey: '3c2db2aef0c7ff26e8911267474a9b2c',
+  //   //   indexName: 'typescriptlang',
+  //   //   inputSelector: '.search input',
+  //   //   handleSelected: customHandleSelected,
+  //   // });
+  // }
   // This extra bit of mis-direction ensures that non-essential code runs after
   // the page is loaded
-  useEffect(() => {
-    setupStickyNavigation()
+  // useEffect(() => {
+  //   setupStickyNavigation()
 
-    // @ts-ignore - this comes from the script above
-    if (window.docsearch) {
-      loadDocSearch();
-    }
-    if (document.getElementById("algolia-search")) return
+  //   // @ts-ignore - this comes from the script above
+  //   if (window.docsearch) {
+  //     loadDocSearch();
+  //   }
+  //   if (document.getElementById("algolia-search")) return
 
-    const searchScript = document.createElement('script');
-    searchScript.id = "algolia-search"
-    const searchCSS = document.createElement('link');
+  //   //const searchScript = document.createElement('script');
+  //   //searchScript.id = "algolia-search"
+  //   //const searchCSS = document.createElement('link');
 
-    searchScript.src = withPrefix("/js/docsearch.js");
-    searchScript.async = true;
-    searchScript.onload = () => {
-      // @ts-ignore - this comes from the script above
-      if (window.docsearch) {
-        loadDocSearch();
+  //   //searchScript.src = withPrefix("/js/docsearch.js");
+  //  // searchScript.async = true;
+  //   searchScript.onload = () => {
+  //     // @ts-ignore - this comes from the script above
+  //     if (window.docsearch) {
+  //       loadDocSearch();
 
-        searchCSS.rel = 'stylesheet';
-        searchCSS.href = withPrefix('/css/docsearch.css');
-        searchCSS.type = 'text/css';
-        document.body.appendChild(searchCSS);
+  //       searchCSS.rel = 'stylesheet';
+  //       searchCSS.href = withPrefix('/css/docsearch.css');
+  //       searchCSS.type = 'text/css';
+  //       document.body.appendChild(searchCSS);
 
-        document.getElementById("search-form")?.classList.add("search-enabled")
-      }
-    }
+  //       document.getElementById("search-form")?.classList.add("search-enabled")
+  //     }
+  //   }
 
-    document.body.appendChild(searchScript);
-  }, []);
+  //   document.body.appendChild(searchScript);
+  // }, []);
 
 
   return (
@@ -96,7 +96,7 @@ export const SiteNav = (props: Props) => {
         </div>
 
 
-        <div className="right above-small">
+        {/* <div className="right above-small">
 
           <nav role="navigation" className="search-section">
             <OpenInMyLangQuickJump />
@@ -108,7 +108,7 @@ export const SiteNav = (props: Props) => {
               </form>
             </div>
           </nav>
-        </div>
+        </div> */}
       </div>
 
       <div id="site-content" />
