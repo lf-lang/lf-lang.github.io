ad936b7a047435e6b97f1547d8dc71018161196e
Anirudh Rengarajan <44007330+arengarajan99@users.noreply.github.com>
2022-04-14 18:53:11 -0700

    Added Support for Multiple PDFS
    Images still not rendering at all in PDF, will add as an action item
    
    Other Action Item: Consolidate targetLanguages into single global variable that all files can pull from.

--- packages/lingua-franca/src/lib/setTargetLanguage.ts
+++ packages/lingua-franca/src/lib/setTargetLanguage.ts
@@ -1,19 +1,19 @@
 import { hasLocalStorage } from "./hasLocalStorage";
 
-const targetLanguages = ["lf-c", "lf-cpp", "lf-py", "lf-ts", "lf-rs"];
+const targetLanguages = ["c", "cpp", "py", "ts", "rs"];
 
 export const setTargetLanguage = (selected) => {
   console.log("Setting target language to " + selected);
   hasLocalStorage && localStorage.setItem("last-selected-target-language", selected)
   for (var target of targetLanguages) {
-    for (var element of document.getElementsByClassName('language-' + target)) {
-      if (element.classList.contains('language-' + selected)) {
+    for (var element of document.getElementsByClassName('language-lf-' + target)) {
+      if (element.classList.contains('language-lf-' + selected)) {
         element.style.display = "block";
       } else {
         element.style.display = "none";
       }
     }
-    for (var element of document.getElementsByClassName(target)) {
+    for (var element of document.getElementsByClassName('lf-' + target)) {
       if (element.classList.contains(selected)) {
         element.style.display = "inline";
       } else {
