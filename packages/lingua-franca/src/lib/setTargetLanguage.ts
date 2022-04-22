857be10716b8cc58241021841dc1a89bc0df960b
eal <eal@eecs.berkeley.edu>
2022-04-22 12:36:33 -0700

    Fixed error with code snippets disappearing

--- packages/lingua-franca/src/lib/setTargetLanguage.ts
+++ packages/lingua-franca/src/lib/setTargetLanguage.ts
@@ -6,14 +6,14 @@ export const setTargetLanguage = (selected) => {
   console.log("Setting target language to " + selected);
   hasLocalStorage && localStorage.setItem("last-selected-target-language", selected)
   for (var target of targetLanguages) {
-    for (var element of document.getElementsByClassName('language-lf-' + target)) {
-      if (element.classList.contains('language-lf-' + selected)) {
+    for (var element of Array.from(document.getElementsByClassName('language-lf-' + target) as HTMLCollectionOf<HTMLElement>)) {
+      if (element.classList.contains('language-' + selected)) {
         element.style.display = "block";
       } else {
         element.style.display = "none";
       }
     }
-    for (var element of document.getElementsByClassName('lf-' + target)) {
+    for (var element of Array.from(document.getElementsByClassName('lf-' + target) as HTMLCollectionOf<HTMLElement>)) {
       if (element.classList.contains(selected)) {
         element.style.display = "inline";
       } else {
