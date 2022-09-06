79608260979822b7304fe55ce46f786b45e73b83
Edward A. Lee <eal@eecs.berkeley.edu>
2022-09-06 11:46:05 -0400

    Clone p

--- packages/lingua-franca/src/templates/pages/publications-and-presentations.tsx
+++ packages/lingua-franca/src/templates/pages/publications-and-presentations.tsx
@@ -23,7 +23,7 @@ const Index: React.FC<Props> = (props) => {
 
 <p>In reverse chronological order: </p>
 <ul>
-<li><p><strong>LET&#39;23</strong>: Edward A. Lee and Marten Lohstroh, "<a href="https://www.icyphy.org/publications/2023_LeeLohstroh/">Generalizing Logical Execution Time</a>", in <i>Principles of Systems Design - Essays Dedicated to Thomas A. Hensinzer on the Occasion of his 60th Birthday</i>, LNCS 13660, July 2023, to appear.
+<li><p><strong>LET&#39;23</strong>: Edward A. Lee and Marten Lohstroh, "<a href="https://www.icyphy.org/publications/2023_LeeLohstroh/">Generalizing Logical Execution Time</a>", in <i>Principles of Systems Design - Essays Dedicated to Thomas A. Hensinzer on the Occasion of his 60th Birthday</i>, LNCS 13660, July 2023, to appear.</p>
 </li>
 <li><p><strong>Xronos&#39;22</strong>: Soroush Bateni, Marten Lohstroh, Hou Seng Wong, Rohan Tabish, Hokeun Kim, Shaokai Lin, Christian Menard, Cong Liu, and Edward A. Lee, "<a href="https://arxiv.org/abs/2207.09555">Xronos: Predictable Coordination for Safety-Critical Distributed Embedded Systems</a>,&quot;	arXiv:2207.09555 [cs.DC], July 19, 2022.</p>
 </li>
