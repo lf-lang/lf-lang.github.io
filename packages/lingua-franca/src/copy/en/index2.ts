a5b0cee226ee2d1b2545f8192f6c895a8b38514b
Marten Lohstroh <marten@berkeley.edu>
2023-09-03 01:06:17 -0700

    Update version number

--- packages/lingua-franca/src/copy/en/index2.ts
+++ packages/lingua-franca/src/copy/en/index2.ts
@@ -4,25 +4,25 @@ export const indexCopy = {
   index_2_byline: "Lingua Franca",
   index_2_summary:
     "Lingua Franca (LF) is a polyglot coordination language built to bring deterministic reactive concurrency and time to mainstream target programming languages (currently C, C++, Python, TypeScript, and Rust). LF is supported by a runtime system that is capable of concurrent and distributed execution of reactive programs that are deployable on the Cloud, the Edge, and even on bare-iron embedded platforms.",
-  index_2_detail: "A Lingua Franca program specifies the interactions between components called reactors. The logic of each reactor is written in plain target code. A code generator synthesizes one or more programs in the target language, which are then compiled using standard tool chains. If the application has exploitable parallelism, then it executes transparently on multiple cores without compromising determinacy. A distributed application translates into multiple programs and scripts to launch those programs on distributed machines. The communication fabric connecting components is synthesized as part of the programs.",
-
+  index_2_detail:
+    "A Lingua Franca program specifies the interactions between components called reactors. The logic of each reactor is written in plain target code. A code generator synthesizes one or more programs in the target language, which are then compiled using standard tool chains. If the application has exploitable parallelism, then it executes transparently on multiple cores without compromising determinacy. A distributed application translates into multiple programs and scripts to launch those programs on distributed machines. The communication fabric connecting components is synthesized as part of the programs.",
 
   // Above the fold
   index_2_cta_install: "Download Lingua Franca",
-  index_2_cta_install_subtitle: "Version 0.4.0",
+  index_2_cta_install_subtitle: "Version 0.5.0",
 
   index_2_cta_download: "On your computer",
   index_2_cta_download_subtitle: "via Github",
 
   // Quick pitch
   index_2_what_is: "What is Lingua Franca?",
-  
+
   index_2_what_is_lf: "Reactor-oriented",
   index_2_what_is_lf_copy: `Reactors are reactive and composable concurrent software components with inputs, outputs, and local state.`,
-  
+
   index_2_trust: "Concurrent",
   index_2_trust_copy: `Reactions to events are concurrent unless there is an explicit dependency between them.`,
-  
+
   index_2_scale: "Deterministic",
   index_2_scale_copy: `Lingua Franca programs are deterministic by default and therefore easy to test.`,
 
