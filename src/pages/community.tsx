9d2241245ebce88766afb85801dd8ec385d99235
霧雨 魔理沙 <nkagamihara@suika.eecs.berkeley.edu>
2023-12-05 10:40:30 +0000

    Restructure community and publication directory to make React server happy

---
+++ src/pages/community.tsx
@@ -0,0 +1,2 @@
+import Community from "@site/src/components/Community";
+export default Community;
---
+++ src/pages/publications.tsx
@@ -0,0 +1,2 @@
+import Publications from "@site/src/components/Publications";
+export default Publications;
