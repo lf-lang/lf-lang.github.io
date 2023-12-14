388270a5a0b86a81c56cb5ef00917480f93ad273
axmmisaka <6500159+axmmisaka@users.noreply.github.com>
2023-12-14 00:16:48 -0800

    Adaptation for new Docusaurus-based website (#187)
    * Replace config git edit URL with new repo's URL
    
    * Add CI for docusaurus
    
    ---------
    
    Co-authored-by: Kagamihara Nadeshiko <nkagamihara@suika.eecs.berkeley.edu>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,7 +41,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/axmmisaka/lf-docs-docusaurus/tree/master/docs/',
+            'https://github.com/lf-lang/lf-lang.github.io/tree/master/',
           beforeDefaultRemarkPlugins: [
             // Honestly, I recommend not using this because I am not confident with my coding skill......
             // TransformDynamicLFFileImportToStatic
